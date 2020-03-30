fcfc1a61c6c9863fd71dddfa59cc29a6b9274832
Tinus Strydom <tinuss@loans-inc.co.za>
2020-03-30 19:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+	var getElement = document.getElementById('app').querySelector(selector);
+	return getElement;
+}
 
+function nestedTarget(){
+  var getElement = document.getElementById('app').querySelector('#nested .target');
+  return getElement;
+}
+
+function increaseRankBy(n){
+  var getElement = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+	for (let i = 0; i < getElement.length; i++) {
+		getElement[i].innerHTML = parseInt(getElement[i].innerHTML)+n ;
+	}
+}
+function deepestChild(){
+	var getElement = document.getElementById('app').querySelectorAll('div#grand-node');
+  for(let i = 0; i < getElement.length; i++){
+    return getElement[i].innerHTML;
+  }
+
+}
